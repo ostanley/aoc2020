1ce772e87235a03b305e5cf956e4485154bed270
Olivia Stanley <oliviawstanley@gmail.com>
2020-12-12 09:34:32 -0500

    quick refactor of shared methods

--- Day12/Day12.py
+++ Day12/Day12.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 
+def turn_bearing(bearing, dir, rotation_angle):
+    rotation = np.array([[0, -1], [1, 0]])
+    if dir == 'R':
+        rotation = -1*rotation
+    turns = int(int(rotation_angle)/90)
+    for t in range(turns):
+        bearing = np.dot(bearing[np.newaxis, :], rotation)
+    return bearing.flatten()
+
+
+def move_in_compass_grid(position, dir, distance):
+    compass_dict = {'N': np.array([1, 0]),
+                    'S': np.array([-1, 0]),
+                    'E': np.array([0, 1]),
+                    'W': np.array([0, -1])}
+    return position + compass_dict[dir]*int(distance)
+
+
 class Ship:
     def __init__(self, position, facing):
         self.position = np.array(position)
@@ -10,26 +28,10 @@ class Ship:
         if line[0] == 'F':
             self.position += int(line[1:])*self.facing
         elif line[0] == 'L' or line[0] == 'R':
-            self.turn_ship(line[0], line[1:])
+            self.facing = turn_bearing(self.facing, line[0], line[1:])
         else:
-            self.position = self.move_in_compass_grid(self.position,
+            self.position = move_in_compass_grid(self.position,
                                                       line[0], line[1:])
-
-    def turn_ship(self, dir, rotation_angle):
-        rotation = np.array([[0, -1], [1, 0]])
-        if dir == 'R':
-            rotation = -1*rotation
-        turns = int(int(rotation_angle)/90)
-        for t in range(turns):
-            self.facing = np.dot(self.facing[np.newaxis, :], rotation)
-        self.facing = self.facing.flatten()
-
-    def move_in_compass_grid(self, value, dir, distance):
-        compass_dict = {'N': np.array([1, 0]),
-                        'S': np.array([-1, 0]),
-                        'E': np.array([0, 1]),
-                        'W': np.array([0, -1])}
-        return value + compass_dict[dir]*int(distance)
 
     def move_to_waypoint(self, waypoint, distance):
         self.position += int(distance)*waypoint.get_relative_position()
@@ -44,25 +46,17 @@ class Ship:
         return sum([abs(x) for x in self.position])
 
 
-class Waypoint(Ship):
+class Waypoint():
     def __init__(self, relative_to_ship):
         self.relative_to_ship = np.array(relative_to_ship)
 
     def update_position(self, line):
         if line[0] == 'L' or line[0] == 'R':
-            self.turn_waypoint_around_ship(line[0], line[1:])
+            self.relative_to_ship = turn_bearing(self.relative_to_ship,
+                                                              line[0], line[1:])
         else:
-            self.relative_to_ship = self.move_in_compass_grid(
+            self.relative_to_ship = move_in_compass_grid(
                 self.relative_to_ship, line[0], line[1:])
-
-    def turn_waypoint_around_ship(self, dir, rotation_angle):
-        rotation = np.array([[0, -1], [1, 0]])
-        if dir == 'R':
-            rotation = -1*rotation
-        turns = int(int(rotation_angle)/90)
-        for t in range(turns):
-            self.relative_to_ship = np.dot(self.relative_to_ship[np.newaxis, :], rotation)
-        self.relative_to_ship = self.relative_to_ship.flatten()
 
     def get_relative_position(self):
         return self.relative_to_ship
@@ -73,6 +67,7 @@ def part1(file):
     with open(file, 'r') as f:
         for l in f.readlines():
             ship.update_position(l.strip())
+            # print(ship.get_position(), ship.get_facing())
 
     print(f'Final ship position is {ship.get_position()}',
           f'with Manhattan Distance {ship.get_manhattan_distance()}')
@@ -87,6 +82,7 @@ def part2(file):
                 ship.move_to_waypoint(waypoint, l.strip()[1:])
             else:
                 waypoint.update_position(l.strip())
+            # print(waypoint.get_relative_position())
 
     print(f'Final ship position is {ship.get_position()}',
           f'with Manhattan Distance {ship.get_manhattan_distance()}')
