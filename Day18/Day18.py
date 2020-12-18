38786a010ffaf09389cdaca1bbf830e568b03029
Olivia Stanley <oliviawstanley@gmail.com>
2020-12-18 14:39:42 -0500

    always be careful when modding lists in place

--- Day18/Day18.py
+++ Day18/Day18.py
@@ -13,41 +13,99 @@ def find_outer_brackets(line, counter):
     return bracket_loc
 
 
-def process_line_p1(line):
-    output = 0
-    counter = 0
-    while counter < len(line):
-        if counter == 0:
-            if line[counter].isnumeric():
-                output = int(line[counter])
-                counter += 1
+def get_left_num(line, i, func):
+    if line[i-1] not in ops:
+        left_num = int(line[i-1])
+        del line[i-1]
+    elif line[i-1] == ')':
+        bkwd_bracket_loc = find_outer_brackets(line.copy()[::-1], len(line)-i)
+        left_bracket_loc = [0, 0]
+        left_bracket_loc[0] = len(line)-bkwd_bracket_loc[1]-1
+        left_bracket_loc[1] = len(line)-bkwd_bracket_loc[0]-1
+        left_num = func(line[left_bracket_loc[0]+1:left_bracket_loc[1]], func)
+        del line[left_bracket_loc[0]:left_bracket_loc[1]+1]
+        i = i - (left_bracket_loc[1]-left_bracket_loc[0])
+    return left_num, i
+
+
+def get_right_num(line, i, func):
+    if line[i+1] not in ops:
+        right_num = int(line[i+1])
+        del line[i+1]
+    elif line[i+1] == '(':
+        right_bracket_loc = find_outer_brackets(line, i+1)
+        right_num = func(line[right_bracket_loc[0]+1:right_bracket_loc[1]], func)
+        del line[right_bracket_loc[0]:right_bracket_loc[1]+1]
+    return right_num, i
+
+
+def process_line_p1(line, func):
+    i = 0
+    ops = ['+', '*', '(', ')']
+    current_line = line.copy()
+    while i < len(line):
+        if i == 0 and line[i] == '(':
+            bracket_loc = find_outer_brackets(line, i)
+            line[i] = func(line[bracket_loc[0]+1:bracket_loc[1]], func)
+            del line[bracket_loc[0]+1:bracket_loc[1]+1]
+            i += 1
+        elif line[i] == '+' or line[i] == '*':
+            right_num, i = get_right_num(line, i, func)
+            left_num, i = get_left_num(line, i, func)
+            if line[i-1] == '+':
+                line[i-1] = right_num + left_num
             else:
-                bracket_loc = find_outer_brackets(line, counter)
-                output += process_line_p1(line[bracket_loc[0]+1:bracket_loc[1]])
-                counter += bracket_loc[1]+1
-        elif line[counter] == '+' or line[counter] == '*':
-            if line[counter+1].isnumeric():
-                if line[counter] == '+':
-                    output += int(line[counter+1])
-                else:
-                    output *= int(line[counter+1])
-                counter += 2
-            elif line[counter+1] == '(':
-                bracket_loc = find_outer_brackets(line, counter+1)
-                if line[counter] == '+':
-                    output += process_line_p1(line[bracket_loc[0]+1:bracket_loc[1]])
-                else:
-                    output *= process_line_p1(line[bracket_loc[0]+1:bracket_loc[1]])
-                counter = bracket_loc[1]+1
-    return output
+                line[i-1] = right_num * left_num
+            if len(line) == 1:
+                return line[0]
+        else:
+            i += 1
+    return line[0]
+
+
+def process_line_p2(line, func):
+    i = 0
+    ops = ['+', '*', '(', ')']
+    current_line = line.copy()
+    while i < len(line):
+        if i == 0 and line[i] == '(':
+            bracket_loc = find_outer_brackets(line, i)
+            line[i] = process_line_p2(line[bracket_loc[0]+1:bracket_loc[1]], func)
+            del line[bracket_loc[0]+1:bracket_loc[1]+1]
+            i += 1
+        if line[i] == '+':
+            right_num, i = get_right_num(line, i, func)
+            left_num, i = get_left_num(line, i, func)
+            line[i-1] = right_num + left_num
+        else:
+            i += 1
+    i = 0
+    if len(line) == 1:
+        return line[0]
+    while (i < len(line)):
+        if line[i] == '*':
+            right_num, i = get_right_num(line, i, func)
+            left_num, i = get_left_num(line, i, func)
+            line[i-1] = right_num * left_num
+        else:
+            i += 1
+    return line[0]
+
 
 if __name__ == '__main__':
     file = "Day18input.txt"
+    ops = ['*', '+', '(', ')']
 
     homework_sum = 0
+    homework_sum2 = 0
     with open(file, 'r') as f:
         for l in f.readlines():
-            print(l, process_line_p1(l.strip().replace(" ", "")))
-            homework_sum += process_line_p1(l.strip().replace(" ", ""))
+            new_l = []
+            for x in l.strip().split(" "):
+                new_l += list(x)
+            homework_sum += process_line_p1(new_l.copy(), process_line_p1)
+            print(l.strip())
+            homework_sum2 += process_line_p2(new_l.copy(), process_line_p2)
 
     print(f'Sum of input lines: {homework_sum}')
+    print(f'Sum of input lines p2: {homework_sum2}')
