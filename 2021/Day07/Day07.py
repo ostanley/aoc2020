6f75d07eb5629107b16768361c7602836c8d1db6
Olivia Stanley <oliviawstanley@gmail.com>
2021-12-07 07:31:01 -0500

    Revert "lazy optimization"
    This reverts commit 123b07b77cebfa2b02d22c606a5f3d65ea7814a4.

--- 2021/Day07/Day07.py
+++ 2021/Day07/Day07.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from statistics import median
 
 
 def calulate_fuel_cost_p1(crab_dict, pos):
@@ -15,13 +14,11 @@ with open("Day07input.txt", "r") as f:
 
     min_fuel_p1 = 10 ** 10
     min_fuel_p2 = 10 ** 10
-    test_positions = range(
-        int(median(crab_dict.keys())) - 10, int(median(crab_dict.keys()) + 10)
-    )
-    for pos_to_test in test_positions:
+    pos_to_test = max(crab_dict.keys())
+    while pos_to_test > min(crab_dict.keys()):
         min_fuel_p1 = min(calulate_fuel_cost_p1(crab_dict, pos_to_test), min_fuel_p1)
         min_fuel_p2 = min(calulate_fuel_cost_p2(crab_dict, pos_to_test), min_fuel_p2)
         pos_to_test -= 1
 
-    print(f"Guess crab physics minimum fuel: {min_fuel_p1}")
+    print(f"Guess crab physics minimum fuel: {min_fuel_p2}")
     print(f"Real crab physics minimum fuel: {min_fuel_p2}")
